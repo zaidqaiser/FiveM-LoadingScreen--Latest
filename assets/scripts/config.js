acab29e0b63de78505b4f43e7c8b36863c34adee
Creative Dev <zaidqaiser152@gmail.com>
2024-06-22 23:28:07 +0500

    fivem-loading-latest

---
+++ assets/scripts/config.js
@@ -0,0 +1,32 @@
+Config = {}; // Don't touch
+
+Config.ServerIP = "localhost:30120";
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "discord", label: "Discord", description: "Join our community, have fun!", icon: "assets/media/icons/discord.png", link: "https://discord.gg/#"},
+    {name: "tiktok", label: "Tiktok", description: "Watch our video here!", icon: "assets/media/icons/tiktok.png", link: "https://www.instagram.com/#"},
+    {name: "Paypal", label: "Instagram", description: "Checkout latest Update or Events !", icon: "assets/media/icons/tebex.png", link: "https://www.instagram.com/#"},
+];
+
+Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
+Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
+
+// Staff list
+Config.Staff = [
+    {name: "Staff#1", description: "Name", color: "#fff", image: "https://cdn.discordapp.com/attachments/1127485698354716723/1253979300420649011/logo.png?ex=6677d2bb&is=6676813b&hm=972cc87d3da1db0c7b727f3cc51c0a0e4c21bd394edf1cc1433399f3abd2f0c7&"},
+    {name: "Staff#2", description: "Name", color: "#fff", image: "https://cdn.discordapp.com/attachments/1127485698354716723/1253979300420649011/logo.png?ex=6677d2bb&is=6676813b&hm=972cc87d3da1db0c7b727f3cc51c0a0e4c21bd394edf1cc1433399f3abd2f0c7&"},
+    {name: "Staff#3", description: "Name", color: "#fff", image: "https://cdn.discordapp.com/attachments/1127485698354716723/1253979300420649011/logo.png?ex=6677d2bb&is=6676813b&hm=972cc87d3da1db0c7b727f3cc51c0a0e4c21bd394edf1cc1433399f3abd2f0c7&"},
+    {name: "Staff#4", description: "Name", color: "#fff", image: "https://cdn.discordapp.com/attachments/1127485698354716723/1253979300420649011/logo.png?ex=6677d2bb&is=6676813b&hm=972cc87d3da1db0c7b727f3cc51c0a0e4c21bd394edf1cc1433399f3abd2f0c7&"},
+
+
+];
+
+// Categories
+Config.Categories = [
+    {label: "Social Networks", default: false},
+    {label: "Staff team", default: true}
+];
+
+// Music
+//Config.Song = "song.mp3";
---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'zaidwebdev'
+description 'LongVilleRp-Loading'
+version '1.0'
+
+files {
+    '*.html',
+    'assets/**/*.*',
+    'assets/**/**/*.*'
+}
+
+client_script 'client.lua'
+
+--loadscreen_manual_shutdown "yes"
+loadscreen 'index.html'
